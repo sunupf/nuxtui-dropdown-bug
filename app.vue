1108531b267d1bc90a4393027c47f3581e1f26c7
sunupf <sunupf@isuk.studio>
2023-07-13 10:27:02 +0700

    feat(test): create test case for keyboar navigation bug on dropdown button

---
+++ app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <UDropdown :items="cardMenu">
+      <UButton :paddded="false" icon="i-heroicons-ellipsis-vertical" variant="ghost" @click="foo(1)"/>
+    </UDropdown>
+  </div>
+</template>
+
+<script setup>
+const cardMenu = [
+    [
+      {
+        label: 'Edit',
+        icon: 'i-heroicons-pencil-square',
+        shortcuts: ['E'],
+        click: () => {
+            console.log('Edit')
+        }
+      }, {
+        label: 'Duplicate',
+        icon: 'i-heroicons-document-duplicate',
+        shortcuts: ['D'],
+        disabled: true
+      }
+    ],
+    [
+      {
+        label: 'Delete',
+        icon: 'i-heroicons-trash',
+        click: () => {
+            pageConfig.confirmationDeleteSurvey = true
+        }
+      }
+    ]
+]
+
+const foo = (num) => {
+  console.log(num)
+}
+</script>
